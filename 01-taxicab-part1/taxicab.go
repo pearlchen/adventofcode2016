1460f22e6f47281a5825bd51953872e5ca3e1ac6
Pearl Chen <dotdotdotspace@gmail.com>
2016-12-04 21:20:30 -0500

    Got rid of giant nested if test
    Learned how to use golang maps, arrays, slices, and structs

--- 01-taxicab-part1/taxicab.go
+++ 01-taxicab-part1/taxicab.go
@@ -10,30 +10,50 @@ import (
 func main() {
 
 	// example routes for tests
-	// route := "R2, L3" (expect 5)
-	// route := "R2, R2, R2" (expect 2)
-	// route := "R5, L5, R5, R3" (expect 12)
+	// route := "R2, L3"         // expect 5
+	// route := "R2, R2, R2"     // expect 2
+	// route := "R5, L5, R5, R3" // expect 12
 
-	// original directions from http://adventofcode.com/2016/day/1 (expect 241)
+	// original directions from http://adventofcode.com/2016/day/1
+	// final answer to expect = 241
 	route := "R1, R1, R3, R1, R1, L2, R5, L2, R5, R1, R4, L2, R3, L3, R4, L5, R4, R4, R1, L5, L4, R5, R3, L1, R4, R3, L2, L1, R3, L4, R3, L2, R5, R190, R3, R5, L5, L1, R54, L3, L4, L1, R4, R1, R3, L1, L1, R2, L2, R2, R5, L3, R4, R76, L3, R4, R191, R5, R5, L5, L4, L5, L3, R1, R3, R2, L2, L2, L4, L5, L4, R5, R4, R4, R2, R3, R4, L3, L2, R5, R3, L2, L1, R2, L3, R2, L1, L1, R1, L3, R5, L5, L1, L2, R5, R3, L3, R3, R5, R2, R5, R5, L5, L5, R2, L3, L5, L2, L1, R2, R2, L2, R2, L3, L2, R3, L5, R4, L4, L5, R3, L4, R1, R3, R2, R4, L2, L3, R2, L5, R5, R4, L2, R4, L1, L3, L1, L3, R1, R2, R1, L5, R5, R3, L3, L3, L2, R4, R2, L5, L1, L1, L5, L4, L1, L1, R1"
 
-	// turn the big string into an array using split using the comma and space as delimiter
+	// turn the `route` string into an array using split using both the comma and space as delimiter
 	steps := strings.Split(route, ", ")
-	// fmt.Println("Number of steps:", len(steps))
+	fmt.Println("Number of steps in route:", len(steps))
 
 	// use x,y cartesian-based coordinates to keep track of path walked
-	var x int64 = 0
-	var y int64 = 0
+	var coords = map[string]int64{"x": 0, "y": 0}
+
+	// define a struct for how to behave for a given direction
+	// TODO: can reduce complexity of right/left if turn to face direction first
+	type behaviour struct {
+		facing string // "N", "E", "S", "W"
+		coord  string // reference to x or y
+		right  int64  // if going right, increment (1) or decrement (-1)
+		left   int64  // if going left, increment (1) or decrement (-1)
+	}
+
+	// calling this array `compass` since the positions of behaviours will "rotate around"
+	compass := []behaviour{
+		{facing: "N", coord: "x", right: 1, left: -1},
+		{facing: "E", coord: "y", right: 1, left: -1},
+		{facing: "S", coord: "x", right: -1, left: 1},
+		{facing: "W", coord: "y", right: -1, left: 1},
+	}
 
 	// default start state is facing north
-	facing := "N"
-	fmt.Println("Start facing: ", facing)
+	var facing behaviour = compass[0]
+	fmt.Println("Start facing: ", facing.facing)
 
+	// start "walking" by looping through the steps
 	for i := 0; i < len(steps); i++ {
 
+		var step = steps[i] //e.g. "R33"
+		fmt.Println("(", i, ")", step, ":")
+
 		// parse `step` into something useable:
-		var step = steps[i]                                  //e.g. "R33"
-		var direction string = step[0:1]                     //e.g. "R"
+		var directionShort string = step[0:1]                //e.g. "R"
 		var blocksString = step[1:len(step)]                 //e.g. "33" (second letter to end of string)
 		blocks, err := strconv.ParseInt(blocksString, 0, 64) //convert string number to a real number e.g. 33
 		if err != nil {                                      // output error if conversion goes wrong
@@ -41,48 +61,34 @@ func main() {
 		}
 
 		// update coords based on whether instructions say
-		// left/right (direction) and how far ('blocks')
-		// TODO: could be optimized
-		if facing == "N" {
-			if direction == "R" {
-				facing = "E"
-				x += blocks
-			} else {
-				facing = "W"
-				x -= blocks
-			}
-		} else if facing == "E" {
-			if direction == "R" {
-				facing = "S"
-				y += blocks
-			} else {
-				facing = "N"
-				y -= blocks
-			}
-		} else if facing == "S" {
-			if direction == "R" {
-				facing = "W"
-				x -= blocks
-			} else {
-				facing = "E"
-				x += blocks
-			}
-		} else if facing == "W" {
-			if direction == "R" {
-				facing = "N"
-				x -= blocks
-			} else {
-				facing = "S"
-				x += blocks
-			}
+		// left/right (`directionShort`) and how far (`blocks`)
+		// TODO: can reduce complexity of right/left if turn to face direction first
+		fmt.Println("   Currently facing: ", facing.facing)
+		if directionShort == "R" {
+			fmt.Println("   Turn RIGHT and walk", blocks, "blocks")
+			coords[facing.coord] += blocks * facing.right
+
+			// update compass clockwise
+			firstSlice, compassTemp := compass[0], compass[1:] //shift (remove the first element)
+			compass = append(compassTemp, firstSlice)          // push (re-add element at end)
+		} else {
+			fmt.Println("   Turn LEFT and walk", blocks, "blocks")
+			coords[facing.coord] += blocks * facing.left
+
+			// update compass counter-clockwise
+			lastSlice, compassTemp := compass[len(compass)-1:], compass[:len(compass)-1] // pop (removes the last item)
+			compass = append(lastSlice, compassTemp...)                                  //unshift (insert element at front)
 		}
 
+		// update to always face direction of first element
+		facing = compass[0]
+
 		// debugging
-		fmt.Println(i, ": ", step, " ", facing, ">>", blocks, " = ", x, ",", y)
+		fmt.Println("   Now at ", coords["x"], coords["y"])
 	}
 
 	// FINAL ANSWER
-	absoluteBlocks := math.Abs(float64(x)) + math.Abs(float64(y))
+	absoluteBlocks := math.Abs(float64(coords["x"])) + math.Abs(float64(coords["y"]))
 	fmt.Println("Total blocks away:", absoluteBlocks)
 
 }
