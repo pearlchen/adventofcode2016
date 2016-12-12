db8efc49f1eb01ab08763d4bd219ce1d92d83199
Pearl Chen <dotdotdotspace@gmail.com>
2016-12-11 22:38:40 -0500

    Day 5 Part 2 solution

--- 05-chess-part2/main.go
+++ 05-chess-part2/main.go
@@ -12,29 +12,41 @@ import (
 func main() {
 
 	// test
-	// doorId := "abc" // expect 18f47a30
+	// doorId := "abc" // expect 05ace8e3
 
 	//real
-	doorId := "ffykfhsq" // expect c6697b55
+	doorId := "ffykfhsq" // expect ???
 
 	// use the Door ID as the seed for the md5 hash
 	// then increase the integer (i), looking for a combined hexademical string starting with "00000"
 	// TODO: better way to do this than increasing the integer???
 	i := 0
-	password := make([]string, 0)
+	password := make([]string, 8)
+	passwordLength := 0
 
-	for len(password) < 8 {
+	for passwordLength < 8 {
 		h := md5.New()
 		io.WriteString(h, doorId)
 		i++
 		io.WriteString(h, strconv.Itoa(i))
 		hash := hex.EncodeToString(h.Sum(nil))
 		if hash[:5] == "00000" {
-			fmt.Println(hash, hash[:5], hash[5:6])
-			password = append(password, hash[5:6])
+			hi, err := strconv.Atoi(hash[5:6])
+			if err != nil {
+				fmt.Println("error converting", hash[5:6])
+				continue
+			}
+			if hi <= 7 {
+				if password[hi] == "" {
+					fmt.Println(hash, hash[:5], hi, hash[6:7])
+					password[hi] = hash[6:7]
+					fmt.Println("    ", password, strings.Join(password, ""))
+					passwordLength = len(strings.Join(password, ""))
+				}
+			}
 		}
 	}
 
-	fmt.Println("PASSWORD:", strings.Join(password, ""))
+	fmt.Println("PASSWORD:", passwordLength, ".", strings.Join(password, ""))
 
 }
